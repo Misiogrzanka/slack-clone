bd5333e40a2b2ca5250440399a65eb32194e4ae6
Misiogrzanka <77200161+Misiogrzanka@users.noreply.github.com>
2021-02-23 23:07:34 +0100

    Developed header + sidebar sections

---
+++ src/data/ChannelsData.js
@@ -0,0 +1,28 @@
+
+
+export const ChannelsData = [
+    {
+        name: "Pizza"
+    },
+    {
+        name: "Neapolitan Pizza"
+    },
+    {
+        name: "Chicago Pizza"
+    },
+    {
+        name: "Roman Pizza"
+    },
+    {
+        name: "Sicilian Pizza"
+    },
+    {
+        name: "Homemade Pizza"
+    },
+    {
+        name: "Frozen Pizza"
+    },
+    {
+        name: "Pineapple Pizza ??"
+    }
+]
